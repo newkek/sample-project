97e2342cc7ece84c50389466df658e99eadbcf31
Kevin Gallardo <kevin.gallardo@datastax.com>
2017-11-22 10:57:49 -0500

    better example.

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,11 +1,12 @@
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 public class MyApp {
 
   public static void main(String[] args) {
-    Graph graph = TinkerGraph.open();
-    GraphTraversalSource g = graph.traversal();
+    GraphTraversalSource g = null;
+
+    // not possible to do:
+//    g.withStrategies();
+    // although it is possible in TinkerPop 3.2.4
   }
 }
